6053552175e4abc24534e6df56700d9784c39015
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-04-03 14:58:44 -0300

    iniciando projeto e organização dos arquivos de layout

---
+++ src/layouts/LayoutMedium.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MediumHeader from '@/components/templates/medium/MediumHeader.vue'
+import MediumAside from '@/components/templates/medium/MediumAside.vue'
+import MediumFooter from '@/components/templates/medium/MediumFooter.vue'
+</script>
+
+<template>
+  <div id="layout-medium">
+    <header>
+      <MediumHeader />
+    </header>
+    <aside>
+      <MediumAside />
+    </aside>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <MediumFooter />
+    </footer>
+  </div>
+</template>
